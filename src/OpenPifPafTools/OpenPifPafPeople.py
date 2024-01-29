90178cd5baabcea9582e3796eecfd90dc729bf34
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-01-29 03:03:12 -0300

    ordenando

--- src/OpenPifPafTools/OpenPifPafPeople.py
+++ src/OpenPifPafTools/OpenPifPafPeople.py
@@ -6,7 +6,7 @@ import OpenPifPafTools.OpenPifPafAnnotations as oppa
 import OpenPifPafTools.OpenPifPafGetData as oppd
 
 
-def get_pil_images_with_people(filepath):
+def get_pil_images_with_people(filepath, factor=1.4,face=False):
     '''
     Lee un archivo de imagen `filepath` y retorna una lista de pil images con una persona por imagen.
     
@@ -19,7 +19,11 @@ def get_pil_images_with_people(filepath):
     
     pil_list=[];
     for annot in annotation:
-        tupla=oppd.get_body_bounding_rectangle(annot.data, factor=1.4);
+        if face==True:
+            tupla=oppd.get_face_bounding_rectangle(annot.data, factor=factor);
+        else:
+            tupla=oppd.get_body_bounding_rectangle(annot.data, factor=factor);
+        
         if not((tupla[0]==0)and(tupla[1]==0)and(tupla[2]==0)and(tupla[3]==0)):
             tupla=oppd.get_valid_bounding_rectangle(tupla, (pil_im.size[0],pil_im.size[1]))
             pil_im_crop = pil_im.crop(tupla);
