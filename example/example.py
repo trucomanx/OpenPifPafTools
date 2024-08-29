0bf2a3ad6ba044c8b5924b339c83598e88134e1e
Fernando Pujaico Rivera <fernando.pujaico.rivera@gmail.com>
2024-08-29 14:29:04 -0300

    ordenando

--- example/example.py
+++ example/example.py
@@ -11,7 +11,7 @@ import PIL
 import OpenPifPafTools.RoiDetector    as opp
 
 
-det=opp.Detector(checkpoint='shufflenetv2k16',body_factor=1.0,face_factor=0.9);
+det=opp.Detector(checkpoint='shufflenetv2k16',body_factor=1.0,face_factor=0.85);
 
 pil_img=PIL.Image.open('../dataset/filename463.jpg');
 
